6245aa8339521fb488a6dd3e16ca56683bfa15cd
chengqianma <cm74@uw.edu>
2019-12-08 22:23:23 -0800

    add test

--- tests/test_generate_disparity_image.py
+++ tests/test_generate_disparity_image.py
@@ -3,6 +3,7 @@ sys.path.append("..")
 from utils import generate_disparity_image
 import os
 import cv2
+import numpy as np
 import unittest
 
 
@@ -17,6 +18,13 @@ class TestCase(unittest.TestCase):
         new_path = path_left[:-4] + "_disparity.png"
         is_exist = os.path.exists(new_path)
         self.assertTrue(is_exist)
+
+    def test_disparity_size(self):
+        generate_disparity_image.generate_disparity_image(path_left, path_right)
+        new_path = path_left[:-4] + "_disparity.png"
+        input_img = cv2.imread(path_left)
+        output_img = cv2.imread(new_path)
+        self.assertEqual(np.shape(input_img), np.shape(output_img), "Disparity size error")
 
 
 if __name__ == '__main__':
