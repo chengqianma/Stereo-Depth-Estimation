78f2a131b69ae92e4a7c655291f8f3d09f65fbd0
chengqianma <cm74@uw.edu>
2019-12-08 22:12:48 -0800

    complete unit tests

--- tests/test_generate_depth_file.py
+++ tests/test_generate_depth_file.py
@@ -2,26 +2,23 @@ import sys
 sys.path.append("..")
 from utils import generate_depth_file
 from utils import config
-import pickle
 import os
-import numpy as np
-import cv2
-
-
-def test_generate_depth_file(path_left, path_right, baseline, focal, pixel_size):
-    generate_depth_file.generate_depth_file(path_left, path_right, config.baseline, config.focal, config.pixel_size)
-    new_path = path_left[:-4] + "_depth_info.pkl"
-    if os.path.exists(new_path):
-        file = open(new_path)
-        file = pickle.load(file)
-        img = cv2.imread(path_left)
-        if np.shape(file) == np.shape(img[:,:,0]):
-            print(np.shape(file))
-        else:
-            print("Wrong depth info generated")
-    else:
-        print("Failed to generate depth info file")
+import unittest
 
 path_left = "../test/000047_10.png"
 path_right = "../test/000047_10_r.png"
-test_generate_depth_file(path_left, path_right, config.baseline, config.focal, config.pixel_size)
+
+
+class TestCase(unittest.TestCase):
+
+    def test_generate_depth_file(self):
+        generate_depth_file.generate_depth_file(path_left, path_right, config.baseline, config.focal, config.pixel_size)
+        new_path = path_left[:-4] + "_depth_info.pkl"
+        is_exist = os.path.exists(new_path)
+        self.assertTrue(is_exist)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_generate_disparity_image.py
+++ tests/test_generate_disparity_image.py
@@ -3,18 +3,21 @@ sys.path.append("..")
 from utils import generate_disparity_image
 import os
 import cv2
+import unittest
 
-
-def test_generate_disparity_image(path_left, path_right):
-    generate_disparity_image.generate_disparity_image(path_left, path_right)
-    new_path = path_left[:-4] + "_disparity.png"
-    if os.path.exists(new_path):
-        img = cv2.imread(new_path)
-        cv2.imshow("Disparity image", img)
-        cv2.waitKey(5000)
-    else:
-        print("Failed to generate disparity image")
 
 path_left = "../test/000047_10.png"
 path_right = "../test/000047_10_r.png"
-test_generate_disparity_image(path_left, path_right)
+
+
+class TestCase(unittest.TestCase):
+
+    def test_generate_disparity_image(self):
+        generate_disparity_image.generate_disparity_image(path_left, path_right)
+        new_path = path_left[:-4] + "_disparity.png"
+        is_exist = os.path.exists(new_path)
+        self.assertTrue(is_exist)
+
+
+if __name__ == '__main__':
+    unittest.main()
