e1581e95db580b098dc8a38cf84233756f1b082e
chengqianma <cm74@uw.edu>
2019-12-09 21:38:54 -0800

    modify ci

--- sdenet/tests/config.py
+++ sdenet/tests/config.py
@@ -9,6 +9,8 @@ FOCAL = 0.006
 PIXEL_SIZE = 4.65 * 0.000001
 # depth range
 RANGE = 1
+# max disparity range
+DISPARITY_RANGE = 192
 # image path
 PATH_LEFT = "../sample_image/000047_10.png"
 PATH_RIGHT = "../sample_image/000047_10_r.png"
--- sdenet/tests/test_generate_disparity_image.py
+++ sdenet/tests/test_generate_disparity_image.py
@@ -28,6 +28,22 @@ class TestCase(unittest.TestCase):
         output_img = cv2.imread(new_path)
         self.assertEqual(np.shape(input_img), np.shape(output_img), "Disparity size error")
 
+    def test_disparity_range(self):
+        '''Test disparity range is smaller than 192(which is the max disparity range 192)'''
+        generate_disparity_image.generate_disparity_image(config.PATH_LEFT, config.PATH_RIGHT)
+        new_path = config.PATH_LEFT[:-4] + "_disparity.png"
+        img = cv2.imread(new_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        max_disparity = max(map(max, img))
+        min_disparity = min(map(min, img))
+        gap = max_disparity - min_disparity
+        if gap <= config.DISPARITY_RANGE:
+            temp = True
+        else:
+            temp = False
+            
+        self.assertTrue(temp)
+
 
 if __name__ == '__main__':
     unittest.main()
